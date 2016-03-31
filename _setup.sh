bbf2c6ef367601eba57317b6245f219339c7e63d
Nima Hejazi <nima.hejazi7@gmail.com>
2016-03-31 16:53:13 -0700

    good to go now...

--- _setup.sh
+++ _setup.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 
-if [-e ~/.goodnews]; then
+if [ -e ~/.goodnews ]; then
     echo "old goodnews file exists...removing...";
     rm -f ~/.goodnews;
 fi
 
-
 cat *.txt > goodnews
-ln -s ./goodnews ~/.goodnews
+ln -s ~/.good-news/goodnews ~/.goodnews
